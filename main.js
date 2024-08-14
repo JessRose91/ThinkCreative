45702365ee0ac3457cf84264f49440661a2313c7
JessRose91 <151757405+JessRose91@users.noreply.github.com>
2024-08-15 09:29:48 +1000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,100 @@
+/*DEFINITIONS OF CREATIVITY, CREATIVE THINKING, AND PROBLEM SOLVING */
+/*Creativity*/
+const dialogCr = document.querySelector('.CreativityDefinition');
+const CreatDef = document.getElementById('OpenCrDef');
+CreatDef.addEventListener('click', () => dialogCr.show());
+/*Creative Thinking*/
+const dialogCt = document.querySelector('.CrThDefinition');
+const CrThDef = document.getElementById('OpenCtDef');
+CrThDef.addEventListener('click', () => dialogCt.show());
+/*Problem-Solving*/
+const dialogPs = document.querySelector('.PrSoDefinition');
+const PrSoDef = document.getElementById('OpenPsDef');
+PrSoDef.addEventListener('click', () => dialogPs.show());
+
+/*FUN FACTS OF CREATIVITY, CREATIVE THINKING, AND PROBLEM SOLVING */
+/*Creativity*/
+const dialogCrFf = document.querySelector('.CreativityFunFacts');
+const CreatFunFacts = document.getElementById('OpenCrFf');
+CreatFunFacts.addEventListener('click', () => dialogCrFf.show());
+/*Creative Thinking */
+const dialogCtFf = document.querySelector('.CreativeThinkingFunFacts');
+const CreaThinkFunFacts = document.getElementById('OpenCtFf');
+CreaThinkFunFacts.addEventListener('click', () => dialogCtFf.show());
+/*Problem Solving*/
+const dialogPsFf = document.querySelector('.ProblemSolvingFunFacts');
+const ProSoFunFacts = document.getElementById('OpenPsFf');
+ProSoFunFacts.addEventListener('click', () => dialogPsFf.show());
+
+/*CREATIVITY VIEW: PROMPTS AND CHALLENGES*/
+const dialogPrCh = document.querySelector('.PromptsChallenges');
+const CrPrChall = document.getElementById('OpenPrCh');
+CrPrChall.addEventListener('click', () => dialogPrCh.show());
+
+const dialogPrCh2 = document.querySelector('.PromptsChallenges2');
+const CrPrChall2 = document.getElementById('OpenPrCh2');
+CrPrChall2.addEventListener('click', () => dialogPrCh2.show());
+
+const dialogPrCh3 = document.querySelector('.PromptsChallenges3');
+const CrPrChall3 = document.getElementById('OpenPrCh3');
+CrPrChall3.addEventListener('click', () => dialogPrCh3.show());
+
+const dialogPrCh4 = document.querySelector('.PromptsChallenges4');
+const CrPrChall4 = document.getElementById('OpenPrCh4');
+CrPrChall4.addEventListener('click', () => dialogPrCh4.show());
+
+const dialogPrCh5 = document.querySelector('.PromptsChallenges5');
+const CrPrChall5 = document.getElementById('OpenPrCh5');
+CrPrChall5.addEventListener('click', () => dialogPrCh5.show());
+
+const dialogPrCh6 = document.querySelector('.PromptsChallenges6');
+const CrPrChall6 = document.getElementById('OpenPrCh6');
+CrPrChall6.addEventListener('click', () => dialogPrCh6.show());
+
+const dialogPrCh7 = document.querySelector('.PromptsChallenges7');
+const CrPrChall7 = document.getElementById('OpenPrCh7');
+CrPrChall7.addEventListener('click', () => dialogPrCh7.show());
+
+const dialogPrCh8 = document.querySelector('.PromptsChallenges8');
+const CrPrChall8 = document.getElementById('OpenPrCh8');
+CrPrChall8.addEventListener('click', () => dialogPrCh8.show());
+
+const dialogPrCh9 = document.querySelector('.PromptsChallenges9');
+const CrPrChall9 = document.getElementById('OpenPrCh9');
+CrPrChall9.addEventListener('click', () => dialogPrCh9.show());
+
+const dialogPrCh10 = document.querySelector('.PromptsChallenges10');
+const CrPrChall10 = document.getElementById('OpenPrCh10');
+CrPrChall10.addEventListener('click', () => dialogPrCh10.show());
+
+const dialogPrCh11 = document.querySelector('.PromptsChallenges11');
+const CrPrChall11 = document.getElementById('OpenPrCh11');
+CrPrChall11.addEventListener('click', () => dialogPrCh11.show());
+
+const dialogPrCh12 = document.querySelector('.PromptsChallenges12');
+const CrPrChall12 = document.getElementById('OpenPrCh12');
+CrPrChall12.addEventListener('click', () => dialogPrCh12.show());
+
+const dialogPrCh13 = document.querySelector('.PromptsChallenges13');
+const CrPrChall13 = document.getElementById('OpenPrCh13');
+CrPrChall13.addEventListener('click', () => dialogPrCh13.show());
+
+const dialogPrCh14 = document.querySelector('.PromptsChallenges14');
+const CrPrChall14 = document.getElementById('OpenPrCh14');
+CrPrChall14.addEventListener('click', () => dialogPrCh14.show());
+
+const dialogPrCh15 = document.querySelector('.PromptsChallenges15');
+const CrPrChall15 = document.getElementById('OpenPrCh15');
+CrPrChall15.addEventListener('click', () => dialogPrCh15.show());
+
+const dialogPrCh16 = document.querySelector('.PromptsChallenges16');
+const CrPrChall16 = document.getElementById('OpenPrCh16');
+CrPrChall16.addEventListener('click', () => dialogPrCh16.show());
+
+const dialogPrCh17 = document.querySelector('.PromptsChallenges17');
+const CrPrChall17 = document.getElementById('OpenPrCh17');
+CrPrChall17.addEventListener('click', () => dialogPrCh17.show());
+
+const dialogPrCh18 = document.querySelector('.PromptsChallenges18');
+const CrPrChall18 = document.getElementById('OpenPrCh18');
+CrPrChall18.addEventListener('click', () => dialogPrCh18.show());
